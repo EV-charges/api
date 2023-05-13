741a87d95dff0554016a6f2257bfcfc1b684720b
Dine512 <dimp.volgin@yandex.ru>
2023-05-13 16:32:02 +0800

    add migration create table

--- migrations/20230509203215_init.up.sql
+++ migrations/20230509203215_init.up.sql
@@ -1,4 +1,16 @@
 CREATE TABLE IF NOT EXISTS places (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    lat NUMERIC(9,7) NOT NULL,
+    lon NUMERIC(9,7) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    city VARCHAR(30),
+    street VARCHAR(70)
+);
+
+CREATE TABLE IF NOT EXISTS id_stations (
+    id SERIAL PRIMARY KEY,
+    place_id INTEGER NOT NULL,
+    inner_id INTEGER NOT NULL,
+    source VARCHAR(30) NOT NULL,
+    FOREIGN KEY (place_id) REFERENCES places (id) ON DELETE CASCADE
 );
